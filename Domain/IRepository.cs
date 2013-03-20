aa79e4f716994356988c2745e2eb60a52cca1ea9
Nguyen Thi Dung <dungnhik5h@gmail.com>
2013-03-20 17:30:15 +0700

    up code

---
+++ Domain/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : IEntity
+    {
+        IQueryable<TEntity> GetAll();
+        void Delete(TEntity entity);
+        void Add(TEntity entity);
+    }
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
