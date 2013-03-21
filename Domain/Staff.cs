14964bbeadbc5f39d626c7cd2dfbec660d336926
Nguyen Thi Dung <dungnhik5h@gmail.com>
2013-03-21 11:24:17 +0700

    Edit properties

--- Domain/Staff.cs
+++ Domain/Staff.cs
@@ -7,5 +7,10 @@ namespace Domain
 {
     public class Staff
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
     }
 }
